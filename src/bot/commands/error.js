7b2bfa7b1d18b3eb5206da05386a01d9758a9215
bigfinfrank <18708515+bigfinfrank@users.noreply.github.com>
2021-05-15 16:14:12 -0400

    no you cannot make the bot post stuff against tos.

--- src/bot/commands/error.js
+++ src/bot/commands/error.js
@@ -1,6 +1,11 @@
 module.exports = {
   func: async (message, suffix) => {
-    message.channel.createMessage(`https://discord.com/channels/${message.channel.guild.id}/590955038684479538/${suffix}`)
+    if (parseInt(suffix) && suffix > 9007199254740000 && suffix < 99999999999999999999n) {
+      message.channel.createMessage(`https://discord.com/channels/345259986303057930/590955038684479538/${suffix}`);
+    }
+    else {
+      message.channel.createMessage("That's not a real error number, liar.");
+    }
   },
   name: 'error',
   description: 'Returns a quick link to an error for the developers',
