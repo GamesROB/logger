df2eabe3cef9171ec83430a9a588704952446fbe
bigfinfrank <18708515+bigfinfrank@users.noreply.github.com>
2021-05-15 12:32:45 -0400

    add old dyno ?error

---
+++ src/bot/commands/error.js
@@ -0,0 +1,11 @@
+module.exports = {
+  func: async (message, suffix) => {
+    message.channel.createMessage(`https://discord.com/channels/${message.channel.guild.id}/590955038684479538/${suffix}`)
+  },
+  name: 'error',
+  description: 'Returns a quick link to an error for the developers',
+  quickHelp: 'Returns a quick link to an error for the developers',
+  examples: `\`${process.env.GLOBAL_BOT_PREFIX}error 123456789123456789\` <- Returns a link to the error with ID 123456789123456789`,
+  type: 'creator',
+  hidden: true
+}
