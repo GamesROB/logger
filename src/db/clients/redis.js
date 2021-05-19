266a52c20092c1360d9b4fa6b41970b43f677b68
bigfinfrank <18708515+bigfinfrank@users.noreply.github.com>
2021-05-19 17:07:37 -0400

    Add REDIS_FAMILY config value & fallback defaults

--- src/db/clients/redis.js
+++ src/db/clients/redis.js
@@ -1,11 +1,11 @@
 const Redis = require('ioredis')
 require('dotenv').config()
 const redis = new Redis({
-  port: process.env.REDIS_PORT, // Redis port
-  host: process.env.REDIS_HOST, // Redis host
-  family: 4, // 4 (IPv4) or 6 (IPv6)
+  port: process.env.REDIS_PORT || 6379, // Redis port
+  host: process.env.REDIS_HOST || localhost, // Redis host
+  family: process.env.REDIS_FAMILY || 4, // 4 (IPv4) or 6 (IPv6)
   password: process.env.REDIS_AUTH,
-  db: process.env.REDIS_DATABASE,
+  db: process.env.REDIS_DATABASE || 0,
 })
 
 
